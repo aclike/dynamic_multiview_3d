82c48cfce76af0a5b96c3a08e41c638a29e9b66f
Owen Jow <owenjow@berkeley.edu>
2017-11-25 18:21:06 -0800

    minor - fix copy path

--- dyn_mult_view/multi_view_model/utils/obj2bam.py
+++ dyn_mult_view/multi_view_model/utils/obj2bam.py
@@ -6,7 +6,7 @@ usage: python obj2bam.py <synset_dir>
 
 OBJ2EGG_CMD = 'python /Users/owenjow/dynamic_multiview_3d/dyn_mult_view/multi_view_model/utils/obj2egg.py {0}/model_normalized.obj'
 EGG2BAM_CMD = 'egg2bam -o {0}/model.bam {0}/model.egg'
-CP_CMD = 'cp {0}/model.bam {0}/../{1}.bam'
+CP_CMD = 'cp {0}/model.bam {0}/../../{1}.bam'
 
 full_cmd = '; '.join((OBJ2EGG_CMD, EGG2BAM_CMD, CP_CMD))
 
